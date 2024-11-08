c6f18025dd14e4b8961d2f0f634572bc2c35c080
siemnijskens <siemnijskens@gmail.com>
2024-11-08 11:47:08 +0100

    exersizes 6 t/m 9 af 10 ongoing

--- src/Players_guide/Day10.java
+++ src/Players_guide/Day10.java
@@ -1,4 +1,5 @@
 package Players_guide;
+
 import java.util.Scanner;
 
 public class Day10 {
@@ -16,26 +17,45 @@ public class Day10 {
         System.out.println("What are the enemy's y coordinates");
         double yCoord = scanner.nextDouble();
 
-      /*  if (5 < Math.abs (xCoord) && yCoord < 100) {
-        // bulk van het programma komt hier
+        double radius = Math.sqrt((xCoord * xCoord) + (yCoord * yCoord));
+        System.out.println(radius);
+
+        if (radius <= 5) {
+            System.out.println("The enemy is at the door");
+        } else if (radius >= 100) {
+            System.out.println("The enemy is out of view");
+        } else if (radius > 5 && radius < 100) {
+
+            if (xCoord == 0 && yCoord > 0) {
+                System.out.println("The enemy is directly to the north");
+            } else if (xCoord == 0 && yCoord < 0) {
+                System.out.println("The enemy is directly to the south");
+
+            } else if (xCoord > 0 && yCoord == 0) {
+                System.out.println("The enemy is directly to the east");
+            } else if (xCoord < 0 && yCoord == 0) {
+                System.out.println("The enemy is directly to the west");
+            }
+            //bulk van de code
+            // als de enemy in range is checkt ie eerst in welk kwadrant hij valt daarna berekent hij de hoek pas
+
+
+
 
         }
-        else if (xCoord < 5 && yCoord < 5) {
-            System.out.println("The enemy is at the door");
 
-        } else if (xCoord > 100 || yCoord > 100) {
-            System.out.println("This enemy is still far away focus on another enemy");
-        }*/
 
-        double radius = Math.sqrt((xCoord*xCoord) + (yCoord*yCoord));
-        System.out.println(radius);
-        double phi = Math.asin (yCoord/radius);
+        double phi = Math.asin(yCoord / radius); //geeft de hoek met het noorden met de klok mee in radialen
         System.out.println(phi);
-        double conversiefactor = 57.2957795; //1 radiaal is zoveel graden
-        System.out.println(phi);
-        double phi2 = phi*conversiefactor;
+        double conversiefactor = 1*180/Math.PI ; //1 radiaal is zoveel graden
+        System.out.println(conversiefactor);
+        double phi2 = phi * conversiefactor;
         System.out.println(phi2);
-    }
+        if (phi2 < 0){
+            phi2 = (phi2 + 360);
 
+            //checken welk quadrant wel werkt en of tangens niet beter werkt
+        }
+    }
 }
 
